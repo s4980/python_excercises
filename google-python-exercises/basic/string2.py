906fa09aa7ab4309ad518f49911b2038df3a55be
Michal <s4980@pjwstk.edu.pl>
2015-10-24 20:03:15 +0100

    String excercises

--- google-python-exercises/basic/string2.py
+++ google-python-exercises/basic/string2.py
@@ -3,6 +3,7 @@
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
 
+
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
